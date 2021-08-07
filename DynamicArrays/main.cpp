b518f88c1f74ebf4bbbf0f64f03ee4729778c4a5
Eugene Stolbin <estolbin@gmail.com>
2021-08-07 14:37:29 +0300

    Dynamic arrays in progress

---
+++ DynamicArrays/main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n] {};
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	delete[] arr;
+}
--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,7 +1,47 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+
+//#define POINTERS_BASICS
+#define POINTERS_AND_ARRAYS
 
 void main() {
 	setlocale(LC_ALL, "Russian");
+
+#ifdef POINTERS_BASICS
 	cout << "Hello Pointers!" << endl;
+
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;
+	cout << pa << endl;
+	cout << *pa << endl;
+	// 'pa' - работаем с адресом
+	// '*pa' - работаем со значением
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+	cout << b << endl;
+	cout << &b << endl;
+	cout << pb << endl;
+	cout << *pb << endl;
+#endif // POINTERS_BASICS
+
+#ifdef POINTERS_AND_ARRAYS
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+
+	cout << *arr << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr + i << "\n";
+	}
+	cout << endl;;
+
+#endif // POINTERS_AND_ARRAYS
+
+
 }
