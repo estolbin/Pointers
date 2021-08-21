9398f597786fb09da33b1b3bf3db33879e4623ea
Eugene Stolbin <estolbin@gmail.com>
2021-08-21 13:09:29 +0300

    Parameters & references

--- DynamicArrays/main.cpp
+++ DynamicArrays/main.cpp
@@ -5,14 +5,9 @@ using namespace std;
 
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
-void push_back(int*& arr, int &p, int new_item);
-void fillArrayFromArray(int*& arr, int n, int* sourceArray);
-void push_front(int*& arr, int& p, int new_item);
-void insert(int*& arr, int& p, int new_item, int indx);
-void pop_back(int*& arr, int& p);
-void pop_front(int*& arr, int& p);
-void erase(int*& arr, int& p, int indx);
-
+int* push_back(int arr[], int& n, int value); // добавляет значение в конец массива
+int* push_front(int arr[], int& n, int value);
+int* pop_back(int arr[], int& n);
 
 void main()
 {
@@ -24,144 +19,19 @@ void main()
 	FillRand(arr, n);
 	Print(arr, n);
 
-	// push_back
-	int new_item = rand() % 100;
-	push_back(arr, n, new_item);
+	arr = push_back(arr, n, 1024);
 	Print(arr, n);
 
-	int new_item2 = rand() % 100; // or cin >> new_item2;
-	push_front(arr, n, new_item2);
+	arr = push_front(arr, n, 2048);
 	Print(arr, n);
 
-	int new_item3 = rand() % 100;
-	insert(arr, n, new_item3, 2);
-	Print(arr, n);
-
-	pop_back(arr, n);
-	Print(arr, n);
-
-	pop_front(arr, n);
-	Print(arr, n);
-
-	int indx = 2;
-	erase(arr, n, indx);
+	arr = pop_back(arr, n);
 	Print(arr, n);
 
 	delete[] arr;
 }
 
-void push_front(int*& arr, int& p, int new_item)
-{
-	int n = p;
-	n++;
-	int* tmpArr = new int[n] {};
-	tmpArr[0] = new_item;
-	for (int i = 1; i < n; i++)
-	{
-		tmpArr[i] = arr[i - 1];
-	}
-	delete[] arr;
 
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void push_back(int*& arr, int &p, int new_item)
-{
-	int n = p;
-	n++;
-	int* tmpArr = new int[n] {};
-	fillArrayFromArray(tmpArr, n, arr);
-	tmpArr[n-1] = new_item;
-	delete[] arr;
-
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void insert(int*& arr, int& p, int new_item, int indx)
-{
-	int n = p;
-	n++;
-	int* tmpArr = new int[n]{};
-
-	for (int i = 0; i < indx; i++)
-	{
-		tmpArr[i] = arr[i];
-	}
-	tmpArr[indx] = new_item;
-	for (int i = indx+1; i < n; i++)
-	{
-		tmpArr[i] = arr[i - 1];
-	}
-	delete[] arr;
-
-
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void pop_back(int*& arr, int& p)
-{
-	int n = p;
-	n--;
-	int* tmpArr = new int[n] {};
-	fillArrayFromArray(tmpArr, n, arr);
-	delete[] arr;
-
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void pop_front(int*& arr, int& p)
-{
-	int n = p;
-	n--;
-	int* tmpArr = new int[n] {};
-	for (int i = 0; i < n; i++)
-	{
-		tmpArr[i] = arr[i + 1];
-	}
-	delete[] arr;
-
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void erase(int*& arr, int& p, int indx)
-{
-	int n = p;
-	n--;
-	int* tmpArr = new int[n] {};
-	for (int i = 0; i < n; i++)
-	{
-		tmpArr[i] = arr[(i >= indx) ? i + 1 : i];
-	}
-	delete[] arr;
-
-	arr = new int[n] {};
-	fillArrayFromArray(arr, n, tmpArr);
-	delete[] tmpArr;
-	p = n;
-}
-
-void fillArrayFromArray(int*& arr, int n, int* sourceArray)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = sourceArray[i];
-	}
-}
 
 void FillRand(int arr[], const int n)
 {
@@ -179,4 +49,54 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	// 1) Создаем буферный массив нужного размера
+	int* buffer = new int[n + 1]{};
+
+	// 2) Копируем содержимое исходного массива в буферный
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	// 3) Освобождаем память, удалив массив arr
+	delete[] arr;
+
+	// 4) Говорим что буфер это наш исходный массив
+	arr = buffer;
+
+	// 5) Только после всего этого в массив arr можно добавить элемент в конец массива
+
+	arr[n] = value;
+
+	// 6) После добавления элемента в конец массива, количество элементов массива увеличивается на 1
+	n++;
+
+	return arr;
+}
+
+int* push_front(int arr[], int& n, int value)
+{
+	int* buffer = new int[n + 1]{};
+
+	buffer[0] = value;
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i + 1] = arr[i];
+	}
+	delete[] arr;
+	arr = buffer;
+	n++;
+
+	return arr;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n]{};
+	for (int i = 0; i < n; i++) buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
 }
